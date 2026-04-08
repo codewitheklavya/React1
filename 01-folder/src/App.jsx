7e4b7b8ce214e4674cc731abfcac17761d15a224
Eklavyasingh7 <codewitheklavya@gmail.com>
2026-04-08 17:07:14 +0530

    01-folder upload

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1">app</h1>
+        <h1 id="child2">app2</h1>
+      </div>
+      <div id="parent2">
+        <h1 id="child3">app3</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
